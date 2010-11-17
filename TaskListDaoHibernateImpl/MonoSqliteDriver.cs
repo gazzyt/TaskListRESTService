f0e34914545c71305a826a96b20e11cc7230fc90
Gary Thomas <gary.thomas@ioko.com>
2010-11-17 14:02:45 +0000

    ported the projects to .net 4

--- TaskListDaoHibernateImpl/MonoSqliteDriver.cs
+++ TaskListDaoHibernateImpl/MonoSqliteDriver.cs
@@ -5,7 +5,7 @@ namespace TaskListDaoHibernateImpl
 	public class MonoSqliteDriver : NHibernate.Driver.ReflectionBasedDriver
 	{
 		public MonoSqliteDriver ()
-			: base("Mono.Data.Sqlite, Version=2.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756", "Mono.Data.Sqlite.SqliteConnection", "Mono.Data.Sqlite.SqliteCommand")
+			: base("Mono.Data.Sqlite, Version=4.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756", "Mono.Data.Sqlite.SqliteConnection", "Mono.Data.Sqlite.SqliteCommand")
 		{
 		}
 		
