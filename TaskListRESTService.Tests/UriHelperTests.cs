8ba6906f0644fd666ad3addd02a1192ff4e88bfd
Gary Thomas <gary.thomas@ioko.com>
2010-11-17 23:18:45 +0000

    factored out the url combining and added unit tests

---
+++ TaskListRESTService.Tests/UriHelperTests.cs
@@ -0,0 +1,22 @@
+using System;
+using NUnit.Framework;
+
+using TaskListRESTService.Utilities;
+
+namespace TaskListRESTService.Tests
+{
+	[TestFixture()]
+	public class UriHelperTests
+	{
+		[TestCase("http://a.b/", "c/d", Result="http://a.b/c/d")]
+		[TestCase("http://a.b/foo", "c/d", Result="http://a.b/foo/c/d")]
+		[TestCase("http://a.b/foo/", "c/d", Result="http://a.b/foo/c/d")]
+		[TestCase("http://a.b/foo", "/c/d", Result="http://a.b/foo/c/d")]
+		[TestCase("http://a.b/foo/", "/c/d", Result="http://a.b/foo/c/d")]
+		public string TestCase (string root, string extra)
+		{
+			return new Uri(root).Append(new Uri(extra, UriKind.Relative)).ToString();
+		}
+	}
+}
+
---
+++ TaskListRESTService/Utilities/UriHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using log4net;
+
+namespace TaskListRESTService
+{
+	static public class UriHelper
+	{
+        //private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly char[] trimChars = {'/'};
+		
+		public static Uri Append(this Uri root, Uri extra)
+		{
+			UriBuilder builder = new UriBuilder(root);
+			
+			if (!builder.Path.EndsWith("/"))
+			{
+				builder.Path = builder.Path + "/";
+			}
+			
+			string extraString = extra.ToString();
+			if (extraString.StartsWith("/"))
+			{
+				extraString = extraString.TrimStart(trimChars);
+			}
+			
+			builder.Path = builder.Path + extraString;
+			
+			return builder.Uri;
+		}
+	}
+}
+
