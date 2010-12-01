6b6d1c5144fd29a4fc0aaff0cc21de6d97375293
Gary Thomas <gary.thomas@ioko.com>
2010-12-01 10:14:46 +0000

    Make nunit tests run against the 4.0 runtime

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mono ~/mono/NUnit-2.5.8.10295/bin/net-2.0/nunit-console.exe TaskListRESTService.nunit
+mono --runtime=v4.0.30319 ~/mono/NUnit-2.5.8.10295/bin/net-2.0/nunit-console.exe TaskListRESTService.nunit
 pkill -f nunit-agent
