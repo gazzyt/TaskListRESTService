69c50152e3a5814a01ee736688d6a388a7a60848
Gary Thomas <gary.thomas@ioko.com>
2010-12-13 12:33:07 +0000

    build solution before running tests

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+xbuild TaskListRESTService.sln
 mono --runtime=v4.0.30319 ~/mono/NUnit-2.5.8.10295/bin/net-2.0/nunit-console.exe TaskListRESTService.Tests/bin/Debug/TaskListRESTService.Tests.dll TaskListDao.Tests/bin/Debug/TaskListDao.Tests.dll
 pkill -f nunit-agent
